37a684a29a58d45bf2334898a5d0bfbc0e93feeb
“vukas86” <vukasbit@gmail.com>
2022-04-01 16:29:43 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -134,6 +134,10 @@ const controlAddRecipe = async function (newRecipe) {
   // upload new recipe data
 };
 
+const newFeature = function () {
+  console.log('Welcome to the new app!');
+};
+
 const init = function () {
   bookmarksView.addHanlerRender(controlBookmarks);
   recipeView.addHandleRender(controlRecipes);
@@ -142,7 +146,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('hi');
+  newFeature();
 };
 
 init();
